6f53953b05b8a636d201600e7cc4559dd2aa512b
HyunKook <gusrnr5153@gmail.com>
2024-09-12 20:53:51 +0900

    [FIX] Level. 3 TodoServiceTest 구현 최종 완료

--- src/test/java/org/example/expert/domain/todo/service/TodoServiceTest.java
+++ src/test/java/org/example/expert/domain/todo/service/TodoServiceTest.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.service;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
@@ -19,12 +20,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -83,4 +89,48 @@ public class TodoServiceTest {
         assertNotNull(result);
     }
 
+    /*일정 단건조회 테스트
+    * 1. 일정 정산 조회
+    * 2. 일정 없을 경우 예외처리
+    * */
+
+    @Test
+    void 일정_단건_정상_조회(){
+        // given
+        AuthUser authUser = new AuthUser(1L, "a@a.com", UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+        long todoId = 1L;
+        Todo todo = new Todo("TITLE1", "contents 1", "sunny", user);
+        ReflectionTestUtils.setField(todo,"id", todoId);
+        given(todoRepository.findByIdWithUser(anyLong())).willReturn(Optional.of(todo));
+
+        // when
+        TodoResponse result = todoService.getTodo(todoId);
+
+        // then
+        assertNotNull(result);
+        assertEquals(todoId, result.getId());
+        assertEquals("TITLE1", result.getTitle());
+        assertEquals("contents 1", result.getContents());
+        assertEquals("sunny", result.getWeather());
+        assertEquals(user.getId(), result.getUser().getId());
+        assertEquals(user.getEmail(), result.getUser().getEmail());
+        assertNotNull(result.getUser());
+    }
+
+    @Test
+    void 일정_없을시_예외처리(){
+        // given
+        AuthUser authUser = new AuthUser(1L, "a@a.com", UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+        long todoId = 1L;
+        Todo todo = new Todo("TITLE1", "contents 1", "sunny", user);
+        ReflectionTestUtils.setField(todo,"id", todoId);
+        given(todoRepository.findByIdWithUser(anyLong())).willReturn(Optional.empty());
+
+        // when & then
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class,
+                () -> todoService.getTodo(todoId));
+        assertEquals("Todo not found", exception.getMessage());
+    }
 }
