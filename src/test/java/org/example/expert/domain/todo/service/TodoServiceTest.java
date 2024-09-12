c7f3f517346c50510f5649f782afa0fa4e5859f5
HyunKook <gusrnr5153@gmail.com>
2024-09-12 17:43:42 +0900

    [FEAT] Level. 3 TodoServiceTest 일정 저장 테스트, 페이징 조회 테스트 (TodoServiceTest 구현 완료)

---
+++ src/test/java/org/example/expert/domain/todo/service/TodoServiceTest.java
@@ -0,0 +1,86 @@
+package org.example.expert.domain.todo.service;
+
+import org.example.expert.client.WeatherClient;
+import org.example.expert.domain.common.annotation.Auth;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.response.TodoResponse;
+import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.todo.repository.TodoRepository;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class TodoServiceTest {
+    @Mock
+    private TodoRepository todoRepository;
+    @Mock
+    private WeatherClient weatherClient;
+    @InjectMocks
+    private TodoService todoService;
+
+    /* 일정 저장 관련 테스트
+     1. 일정 저장, 날씨 호출 테스트
+     */
+
+    @Test
+    void 일정_저장(){
+        // given
+        AuthUser authUser = new AuthUser(1L, "a@a.com", UserRole.USER);
+        User user = User.fromAuthUser(authUser);  // 일정을 만든 유저
+        String weather = "sunny";
+        TodoSaveRequest request = new TodoSaveRequest("Test Title", "Test Contents");
+        Todo todo = new Todo(request.getTitle(), request.getContents(), weather, user);
+
+        given(weatherClient.getTodayWeather()).willReturn(weather);
+        given(todoRepository.save(any(Todo.class))).willReturn(todo);
+
+        // when & then
+        TodoSaveResponse result = todoService.saveTodo(authUser, request);
+
+        assertNotNull(result);
+    }
+
+    /* 일정 페이징 조회 테스트
+    1. 일정 페이징 정상 조회 테스트
+    2. 일정 조회 간 요청한 페이지가 데이터가 없는 경우
+    3. 페이지 요청이 유효하지 않은 경우
+     */
+    @Test
+    void 일정_페이징_정상_조회(){
+        // given
+        AuthUser authUser = new AuthUser(1L, "a@a.com", UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+        Todo todo = new Todo("TITLE1", "contents 1", "sunny", user);
+        Todo todo2 = new Todo("TITLE2", "contents 2", "sunny", user);
+        int page = 1;
+        int size = 2;
+        Pageable pageable = PageRequest.of(page, size);
+        List<Todo> todoList = List.of(todo, todo2);
+        Page<Todo> todos = new PageImpl<>(todoList , pageable,2);
+
+        given(todoRepository.findAllByOrderByModifiedAtDesc(any(Pageable.class))).willReturn(todos);
+
+        // when & then
+        Page<TodoResponse> result = todoService.getTodos(page,size);
+        assertNotNull(result);
+    }
+
+}
